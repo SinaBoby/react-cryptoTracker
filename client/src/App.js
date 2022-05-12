39fb2ebae6321889042bb3ece075feacc22147c6
SinaBoby <sinaboby91@yahoo.com>
2022-05-12 04:44:07 +0200

    TechnicalWidget component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { useEffect } from 'react';
+
+import TechnicalWidget from './components/TechnicalWidget';
 
 
 function App() {
-  useEffect(() => {
-    
-    (async () => {
-      try{
-const response = await fetch('/api/global')
-const json = await response.json()
-console.log(json)
 
-      }catch(error){
-        console.log(error)
-
-      }
-    })()
-  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TechnicalWidget/>
     </div>
   );
 }
